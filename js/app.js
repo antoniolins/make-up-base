4d49ad75f551dcd7e3da3a5740ab8f0cfca3550c
antoniolins <antoniolins@uol.com.br>
2022-07-29 19:25:45 -0300

    Implementando template

--- js/app.js
+++ js/app.js
@@ -1,65 +1,50 @@
-
+const productCardTemplate = document.querySelector("[data-product-template]")
 
 //EXEMPLO DO CÓDIGO PARA UM PRODUTO
-var url = ''
-var products = [{}]
-url = 'http://makeup-api.herokuapp.com/api/v1/products.json';
 
+// var url = url = 'https://makeup-api.herokuapp.com/api/v1/products.json';
 
+let url = 'data/products.json'
+let products = []
 
-
-function selectType() {
-
-  const selecter = document.getElementById('filter-type')
-  const valueSelected = selecter.options[selecter.selectedIndex].value
-
-
-  const element = document.getElementById('div-product');
-  element.remove(); 
-
-  let productsx = products.filter((product) => product.product_type === valueSelected);
-
-  var html = '';
-   productsx.map((product) => (
-    html += productItem(product)
-  ))
-
-  mycatalog = document.getElementById("mycatalog");
-
-  mycatalog.innerHTML = html
-
-
-}
 
 function productItem(product) {
+  
 
-  var item = `<div id="div-product" class="product" data-name=${product.name} data-brand=${product.brand} data-type=${product.product_type} tabindex="508">
-  <figure class="product-figure">
-    <img src="${product.image_link}" width="215" height="215" alt="NYX Mosaic Powder Blush Paradise" onerror="javascript:this.src='img/unavailable.png'">
-  </figure>
-  <section class="product-description">
-    <h1 class="product-name">${product.name} </h1>
-    <div class="product-brands">
-          <span class="product-brand background-brand">${product.brand}</span>
-          <span class="product-brand background-price">R$ ${product.price * 4.5}  </span></div>
-  </section>
+  const productCard = productCardTemplate.content.cloneNode(true).children[0]
+  const header = Productcard.querySelector("[data-name]")
+  const header = Productcard.querySelector("[data-name]")
+  const header = Productcard.querySelector("[data-name]")
+  const header = Productcard.querySelector("[data-name]")
+  const header = Productcard.querySelector("[data-name]")
 
-  // CARREGAR OS DETALHES
+  // var item = `<div id="div-product" class="product" data-name=${product.name} data-brand=${product.brand} data-type=${product.product_type} tabindex="508">
+  // <figure class="product-figure">
+  //   <img src="${product.image_link}" width="215" height="215" alt="NYX Mosaic Powder Blush Paradise" onerror="javascript:this.src='img/unavailable.png'">
+  // </figure>
+  // <section class="product-description">
+  //   <h1 class="product-name">${product.name} </h1>
+  //   <div class="product-brands">
+  //         <span class="product-brand background-brand">${product.brand}</span>
+  //         <span class="product-brand background-price">R$ ${(product.price * 5.5).toFixed(2)}</span></div>
+  // </section>
+
+  // // CARREGAR OS DETALHES
 
   
-  </div>`;
-  return item
+  // </div>`;
+  // return item
 
 }
 
-async function renderProducts(productsx) {
-
-
-  let = products = productsx
+async function renderProducts(products) {
 
   var html = '';
   await products.map((product) => (
-    html += productItem(product)
+
+        // html += ( product.price > 0 ? productItem(product) : "")
+
+       html  += productItem(product)
   ))
 
   mycatalog = document.getElementById("mycatalog")
@@ -68,14 +53,17 @@ async function renderProducts(productsx) {
 
 }
 
-async function getProducts() {
+async function getProducts()  {
 
+const response =  await fetch(url, {method: 'GET', mode: 'no-cors'})
 
-
-  fetch(url)
-    .then(response => response.json())
-    .then(productsx => (renderProducts(productsx)))
-    .catch(erro => console.log(erro))
+      if (!response.ok){
+          throw new Error (`HTTP error ! status: ${response.status}`)
+      }
+    
+    products = await response.json(); // extract data as a JSON Object from response
+  
+    renderProducts(products)
 
 }
 
@@ -110,7 +98,46 @@ function loadDetails(product) {
 }
 
 
-getProducts();
-renderProducts(products);
+function selectType() {
 
 
+  const selecter = document.getElementById('filter-type')
+  var valueSelected = selecter.options[selecter.selectedIndex].value
+
+  if (valueSelected == "todos" )  {
+      renderProducts(products)
+      return   
+  } 
+     // Filtra seleção 
+      myrowset = products.filter((product) => product.product_type == valueSelected);
+
+      renderProducts(myrowset)
+
+}
+
+
+function mySearch() {
+
+  const searchbox = document.getElementById("filter-name").value.toLowerCase()
+  const storeitems = document.getElementById("mycatalog")
+  const myproducts = document.querySelectorAll('.product')
+
+  console.log('Horray! Someone wrote "' + searchbox + '"!');
+
+    myproducts.forEach((product, i) => {
+
+      let productName = document.getElementsByTagName('h1')[i].textContent.toLowerCase()
+
+      if(productName) {
+
+              if(productName.indexOf(searchbox) > -1 ) 
+                 {myproducts[i].style.display = "" 
+                  console.log(productName)}
+              else { myproducts[i].style.display = "none"}
+        }
+    })
+
+  }
+  
+
+getProducts()
